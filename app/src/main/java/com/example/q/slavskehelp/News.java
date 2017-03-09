5571f1a6957dc23226550728d959e31607c420fc
Mykola Hreb <koliahreb@meta.ua>
2017-03-09 20:49:00 +0200

    before create app bar

--- app/src/main/java/com/example/q/slavskehelp/News.java
+++ app/src/main/java/com/example/q/slavskehelp/News.java
@@ -2,6 +2,7 @@ package com.example.q.slavskehelp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class News extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class News extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+    }
+
+    public void onclickmenuTest(View view) {
+        setContentView(R.layout.activity_left_menu);
     }
 }
