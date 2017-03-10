5acb0ea91e444cfe96f41b5853349bb8dc6bea47
Mykola Hreb <koliahreb@meta.ua>
2017-03-10 12:41:14 +0200

    hide action bar

--- app/src/main/java/com/example/q/slavskehelp/Left_menu.java
+++ app/src/main/java/com/example/q/slavskehelp/Left_menu.java
@@ -22,14 +22,6 @@ public class Left_menu extends AppCompatActivity
         setContentView(R.layout.activity_left_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -37,7 +29,6 @@ public class Left_menu extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
