ae3ca0248267714b7694124c6f307ad38e255164
Mykola Hreb <koliahreb@meta.ua>
2017-03-10 15:59:43 +0200

    home button dont click

--- app/src/main/java/com/example/q/slavskehelp/Left_menu.java
+++ app/src/main/java/com/example/q/slavskehelp/Left_menu.java
@@ -3,6 +3,7 @@ package com.example.q.slavskehelp;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,23 +13,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Left_menu extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    private ActionBarDrawerToggle toggle;
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_left_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer,toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(
+                this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
@@ -61,7 +64,15 @@ public class Left_menu extends AppCompatActivity
         if (id == R.id.action_settings) {
             return true;
         }
-
+        if(toggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawer.openDrawer(GravityCompat.START);  // OPEN DRAWER
+                return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 
@@ -88,5 +99,9 @@ public class Left_menu extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void onclickmenuTest(View view) {
+        drawer.openDrawer(GravityCompat.START);
     }
 }
