66ec8ea4e140b8d366c247802ee7aa92cec7d6e2
Mykola Hreb <koliahreb@meta.ua>
2017-03-13 23:26:47 +0200

    registration page

--- app/src/main/java/com/example/q/slavskehelp/News.java
+++ app/src/main/java/com/example/q/slavskehelp/News.java
@@ -1,13 +1,20 @@
 package com.example.q.slavskehelp;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class News extends AppCompatActivity {
+public class News extends AppCompatActivity
+       {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
@@ -25,11 +32,32 @@ public class News extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
-        if(mToggle.onOptionsItemSelected(item))
-        {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        if (mToggle.onOptionsItemSelected(item))
             return true;
-        }
+
         return super.onOptionsItemSelected(item);
     }
-}
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public void news_activity_start(MenuItem item) {
+        Intent newsIntent=new Intent(News.this, News.class);
+        startActivity(newsIntent);
+
+    }
+
+           public void login_activity_start(MenuItem item) {
+               Intent newsIntent=new Intent(News.this, Login.class);
+               startActivity(newsIntent);
+           }
+       }
--- app/src/main/java/com/example/q/slavskehelp/Registration.java
+++ app/src/main/java/com/example/q/slavskehelp/Registration.java
@@ -1,19 +1,26 @@
 package com.example.q.slavskehelp;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
-public class Registration extends AppCompatActivity {
+public class Registration extends AppCompatActivity
+       {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
         mDrawerLayout=(DrawerLayout) findViewById(R.id.drawer_layout);
         mToggle=new ActionBarDrawerToggle(this, mDrawerLayout, R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         mDrawerLayout.addDrawerListener(mToggle);
@@ -23,12 +30,32 @@ public class Registration extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
-        if(mToggle.onOptionsItemSelected(item))
-        {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        if (mToggle.onOptionsItemSelected(item))
             return true;
-        }
+
         return super.onOptionsItemSelected(item);
     }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public void news_activity_start(MenuItem item) {
+        Intent newsIntent=new Intent(Registration.this, News.class);
+        startActivity(newsIntent);
+
+    }
+           public void login_activity_start(MenuItem item) {
+               Intent newsIntent=new Intent(Registration.this, Login.class);
+               startActivity(newsIntent);
+           }
 }
 
