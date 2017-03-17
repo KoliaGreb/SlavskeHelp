cc61dea54c2ac25e45faff7de5ad992cd623c351
Mykola Hreb <koliahreb@meta.ua>
2017-03-17 13:15:04 +0200

    before work with db

--- app/src/main/java/com/example/q/slavskehelp/TestActivity.java
+++ app/src/main/java/com/example/q/slavskehelp/TestActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.telecom.Connection;
 import android.view.View;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import myPackage.Connection.ConnectionClass;
 
 public class TestActivity extends AppCompatActivity {
@@ -14,16 +17,24 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
     }
-
     public void dbTest(View view) {
         ConnectionClass connectionClass=new ConnectionClass();
         java.sql.Connection connection=connectionClass.CONN();
         if(connection==null)
             return; // тут ексепшен напишег
-
+        try
+        {
+        String SQL1="";
+        PreparedStatement preparedStmt = connection.prepareStatement(SQL1);
+        preparedStmt.executeUpdate();
+    }
+          		   catch (SQLException e) {
+        e.printStackTrace();
+    }
         //тут повинен робочий код. все ізі
         //підключення на вічному циклі у пошуках мережі
         // вийде з пошуку тільки сервер рішає, то 120 сек
         //все
     }
+
 }
